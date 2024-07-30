e1f22352433859a9ea0b82fc2e3db28f509b7bbc
Kerryghan <kerryghan.relto.etudiant@pm.me>
2024-07-30 18:17:45 +0200

    Exercise 2.2 in the pocket !

--- P1/P1C2.2/Program.cs
+++ P1/P1C2.2/Program.cs
@@ -1,5 +1,6 @@
 ﻿int savings = 3000;
 
 // TODO: Modify the following statement to use the += shortcut operator	
-savings = savings + 500;
+savings += 500;
 Console.WriteLine("Your savings amount is: " + savings);
+// Hurayyy !
