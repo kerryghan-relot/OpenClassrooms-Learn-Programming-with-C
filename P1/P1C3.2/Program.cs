335574bdf769c2b259833f8809e9b2c2c9d21bbc
Kerryghan <kerryghan.relto.etudiant@pm.me>
2024-08-01 08:54:51 +0200

    Finish Exercise 3.2!

--- P1/P1C3.2/Program.cs
+++ P1/P1C3.2/Program.cs
@@ -2,7 +2,14 @@
 string secondString = "cats";
 
 // TODO: create a new string variable called animals that says 'I love both dogs and cats!'
+string animals = "I love both " + firstString + " and " + secondString + "!";
 
 
 // Display the result 
 Console.WriteLine(animals);
+
+string a = "1";
+int b = 2;
+// Cannot convert string to int
+// int c = (int)a;
+Console.WriteLine(a + b);
