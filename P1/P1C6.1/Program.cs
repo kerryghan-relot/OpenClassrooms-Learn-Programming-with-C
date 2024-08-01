6b58d878a1aabb832ab2d67318808a4a6bd261b0
Kerryghan <kerryghan.relto.etudiant@pm.me>
2024-08-01 21:20:14 +0200

    Finish writing my first rigourous Hello World program in C#! Finished exercise 2.1

--- P1/P1C6.1/Program.cs
+++ P1/P1C6.1/Program.cs
@@ -2,9 +2,17 @@
 
 string spy = "I'm a spy";
 // TODO: Remove the curly brackets { and }
-{
-    string insideCurlyBrackets = "I'm an insider";
-}
+string insideCurlyBrackets = "I'm an insider";
+
 Console.WriteLine(root);
 Console.WriteLine(spy);
 Console.WriteLine(insideCurlyBrackets);
+
+const string expected = "e";
+Console.WriteLine(expected);
+
+
+IDictionary<string, int> frequency = new Dictionary<string, int>();
+frequency.Add("tuesday", 2);
+frequency.Remove("tuesday");
+
---
+++ P2/P2C1.1/Hello/HelloWorld.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Hello
+{
+	/// <summary>
+	/// Declare the class we will be working in for our first (rigorous) Hello World program.
+	/// </summary>
+	/// <remarks>This was shown it Part 2 Chapter 1 of the OpenClassrooms C# course</remarks>
+	public class HelloWorld
+	{
+        /// <summary>
+        /// Simply get the program running; this is where the program starts
+        /// </summary>
+        /// <param name="args">arguments to pass to the main function</param>
+        public static void Main(string[] args)
+		{
+			SayHelloTo("World");
+		}
+		/// <summary>
+		/// Displays a nice "Hello World!" string in the console !
+		/// </summary>
+		/// <param name="recipient">Whom you want to say hello to</param>
+		private static void SayHelloTo(string recipient)
+		{
+			Console.WriteLine("Hello " + recipient + "!");
+		}
+	}
+}
