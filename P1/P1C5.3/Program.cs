c7d6de8e7c2355c7ed6824f21f8d3675efa454b1
Kerryghan <kerryghan.relto.etudiant@pm.me>
2024-08-01 13:39:58 +0200

    Finish exercise 5.3

--- P1/P1C5.3/Program.cs
+++ P1/P1C5.3/Program.cs
@@ -5,10 +5,10 @@ ingredients.Add("butter");
 ingredients.Add("vanilla");
 
 // TODO: add another ingredient to the set
-
+ingredients.Add("flour");
 
 // TODO: now remove vanilla from the set
-
+ingredients.Remove("vanilla");
 
 // Display the resuting set
 Console.WriteLine("Here is our ingredients list");
