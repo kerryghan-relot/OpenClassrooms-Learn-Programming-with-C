4a4ff1eadfdbc77e0b50dc5c3bfcda18c77195a5
Kerryghan <kerryghan.relto.etudiant@pm.me>
2024-08-01 11:53:56 +0200

    Finish exercise 5.2 with some tests as well

--- P1/P1C5.2/Program.cs
+++ P1/P1C5.2/Program.cs
@@ -1,17 +1,27 @@
 ﻿// TODO: Replace the ?? by the proper code to create an ArrayList of string
-IList <??> guests = new ?? ();
+IList <string> guests = new List<string>();
+// This would work as well:
+// List<string> guests = new List<string>();
+// IList<string> guests = ["1", "2", "3"];
+
+
 
 // TODO: Add Joey, Martin and Marie to the list
+guests.Add("Joey");
+guests.Add("Martin");
+guests.Add("Marie");
 
 
 // TODO: Complete the following statement by replacing the ?? to display the size of the list
-Console.WriteLine(guests.??);
+Console.WriteLine(guests.Count);
 
 // TODO: Replace Martin with John in the list
-
+guests[1] = "John";
 
 // TODO: Remove Joey from the list
-
+guests.RemoveAt(0);
+bool a = guests.Remove("Joey");  // This also works. We can observe that if the value is not in the list, nothing happens, the method returns False
+Console.WriteLine(a);
 
 // Display the content of the list
 Console.WriteLine("The guests are:");
