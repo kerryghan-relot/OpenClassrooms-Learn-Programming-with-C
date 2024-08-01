62ab15f77d60b5b191552398601fedc8d79fa2a7
Kerryghan <kerryghan.relto.etudiant@pm.me>
2024-08-01 13:51:25 +0200

    Finish exercise 5.4!

--- P1/P1C5.4/Program.cs
+++ P1/P1C5.4/Program.cs
@@ -1,19 +1,20 @@
 ﻿IDictionary<string, int> months = new Dictionary<string, int>();
 
 // TODO: replace names with constant (final) String variables
-months.Add("June", 6);
-months.Add("September", 9);
-months.Add("March", 5);
+const string keyJune = "June", keySeptembre = "Septembre", keyMarch = "March";
+months.Add(keyJune, 6);
+months.Add(keySeptembre, 9);
+months.Add(keyMarch, 5);
 
 // TODO: Fix March with the proper value (3)
-
+months[keyMarch] = 3;
 
 // TODO: Remove June
-
+months.Remove(keyJune);
 
 // Display the contents of the dictionary
-Console.WriteLine("This is our ingredients list:");
+Console.WriteLine("This is our months list:");
 foreach (KeyValuePair<string, int> keyPair in months)
 {
-    Console.WriteLine(keyPair.Key.ToString() + " is month number " + keyPair.Value + " of the year ");
+    Console.WriteLine(keyPair.Key + " is month number " + keyPair.Value + " of the year ");
 }
