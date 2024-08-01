8d4a3ce99355306d364cc34084c0e7bd1c0edfdd
Kerryghan <kerryghan.relto.etudiant@pm.me>
2024-08-01 09:52:01 +0200

    Finish Exercise 4. Still have some stuff to figure out with the classes and stuff, but that's part of the learning process!

--- P1/P1C4.1/Program.cs
+++ P1/P1C4.1/Program.cs
@@ -1,8 +1,12 @@
-﻿// TODO Step 1 - instantiate an object of class Book and assign it to a variable named myBook
+﻿using P1C4; // For some reason, we must include that line to make our program work.
 
+// TODO Step 1 - instantiate an object of class Book and assign it to a variable named myBook
+Book myBook = new Book();
 
 // TODO Step 2 - assign a value to the title, author and numberOfPages fields of your object.
-
+myBook.Title = "The Title";
+myBook.Author = "The Author";
+myBook.NumberOfPages = 415;
 
 // Display the values 
 Console.WriteLine("The title of the book is " + myBook.Title);
