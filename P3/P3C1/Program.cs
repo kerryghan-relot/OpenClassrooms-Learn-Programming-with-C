6b22ef3c3c9dc694114c009cfdba4e5409c7caa9
Kerryghan <kerryghan.relto.etudiant@pm.me>
2024-08-02 19:51:13 +0200

    Commit of the exercise 3.1

---
+++ P3/P3C1/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+/// I used that project to try things allong the chapter 1 in part 3. No real exercise here, just testing some stuff with constant, references, ...
+
+const int c = 5;
+
+Console.WriteLine(adder(c, 9));
+
+int adder(int a, int b)
+{
+    a++;
+    return a + b;
+}
