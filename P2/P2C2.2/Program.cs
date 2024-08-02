c08b55dccd720545d9d258844bfd2e23a9a84e16
Kerryghan <kerryghan.relto.etudiant@pm.me>
2024-08-02 09:27:25 +0200

    Finish Exercise 2.2

--- P2/P2C2.2/Program.cs
+++ P2/P2C2.2/Program.cs
@@ -3,23 +3,21 @@
 Color selectedColor = Color.Green;
 
 // TODO: Replace the if/else sequence with an equivalent switch statement that operates according to the value of the selectedColor variable
-if (selectedColor == Color.Blue)
+switch (selectedColor)
 {
-    Console.WriteLine("The color is blue");
-}
-else if (selectedColor == Color.Green)
-{
-    Console.WriteLine("The color is green");
-}
-else if (selectedColor == Color.Red)
-{
-    Console.WriteLine("The color is red");
-}
-else if (selectedColor == Color.Yellow)
-{
-    Console.WriteLine("The color is yellow");
-}
-else
-{
-    Console.WriteLine("We do not know the color!");
+    case Color.Blue:
+        Console.WriteLine("The color is blue");
+        break;
+    case Color.Green:
+        Console.WriteLine("The color is green");
+        break;
+    case Color.Red:
+        Console.WriteLine("The color is red");
+        break;
+    case Color.Yellow:
+        Console.WriteLine("The color is yellow");
+        break;
+    default:
+        Console.WriteLine("We do not know the color!");
+        break;
 }
